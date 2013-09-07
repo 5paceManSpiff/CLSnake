531e15e2802c751b190c494900ff09cfaf63e4b7
5paceManSpiff <alspore@gmail.com>
2013-09-06 23:38:30 -0400

    added comments to snake.py

--- src/snake.py
+++ src/snake.py
@@ -2,7 +2,7 @@ import curses
 import random
 
 class Part(object):
-
+	"""Main component of Snake and Food."""
 	x = y = 0
 
 	def __init__(self, y, x):
@@ -10,7 +10,7 @@ class Part(object):
 		self.x = x
 
 class Food(Part):
-
+	"""Food class that can display at given Part coordinates."""
 	screen = None
 
 	def __init__(self, y, x, maxy, maxx, screen):
@@ -20,15 +20,17 @@ class Food(Part):
 		self.maxx = maxx
 
 	def display(self):
+		"""Displays part with \"*\" character."""
 		self.screen.addstr(self.y, self.x, '*')
 
 	def new(self):
+		"""Generates new part coordinates out of terminal dimensions."""
 		self.screen.addstr(self.y, self.x, ' ')
 		self.x = random.randrange(0, self.maxx)
 		self.y = random.randrange(0, self.maxy)
 
 class Snake(object):
-
+	"""Contains the part data of the snake."""
 	parts = []
 	screen = None
 
@@ -40,14 +42,17 @@ class Snake(object):
 			self.parts.append(Part(5, 15 - i))
 
 	def display(self):
+		"""Displays all of the snake parts."""
 		for i in range(len(self.parts)):
 			self.screen.addstr(self.parts[i].y, self.parts[i].x, 'x')
 
 	def delete(self):
+		"""Deletes the last snake part in the array."""
 		self.screen.addstr(self.parts[-1].y, self.parts[-1].x, ' ')
 		del self.parts[-1]
 
 	def move(self, facing, direction=None):
+		"""Creates a new snake part in the given direction to add the appearence of movement."""
 
 		if direction == 'u' and facing != 'd':
 			self.parts.insert(0, Part(self.parts[0].y - 1, self.parts[0].x))
