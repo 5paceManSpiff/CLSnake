291745e1f63e442df355071c7cdaadc581f3324f
5paceManSpiff <alspore@gmail.com>
2013-09-06 23:31:56 -0400

    added comments to curwrap.py

--- src/curwrap.py
+++ src/curwrap.py
@@ -1,10 +1,12 @@
 import curses
 
 def startC(stdscr):
+	"""Wraps curses initialization."""
 	curses.noecho()
 	curses.cbreak()
 	curses.curs_set(0)
 def endC(stdscr):
+	"""Wraps curses exit."""
 	curses.curs_set(1)
 	curses.echo()
 	curses.nocbreak()
